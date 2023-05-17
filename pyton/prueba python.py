a19ec44882d23794d6885fb8b77cc3603f79827a
AbransZ <seisuke1924@gmail.com>
2023-05-16 22:42:17 -0400

    committ inicial

---
+++ pyton/prueba python.py
@@ -0,0 +1,30 @@
+"""print("suma")
+
+
+numero_1 = 5
+numero_2 = 2
+
+suma = numero_1+numero_2
+
+
+print("el resultado de", numero_1, " mas ", numero_2, " es: ", suma)
+
+
+print("multipicacion: ")
+numero_1 = 5
+numero_2 = 2
+multi = numero_1*numero_2
+print("el resultado de", numero_1, " por ", numero_2, " es: ", multi)
+
+print("modulo: ")
+numero_1 = 5
+numero_2 = 2
+mod = numero_1 % numero_2
+print("el resultado de", numero_1, " modulo de ", numero_2, " es: ", mod)
+"""
+valor1 = input("ingrese el valor del primer numero\n")
+valor2 = input("ingrese el valor del segundo numero\n")
+
+potencia = valor1**valor2
+
+print("el resultado de la potencia es: " + str(potencia))
