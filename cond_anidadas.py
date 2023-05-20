3a993186f2b2c9c78a5b96ea77d97dd10a2fa305
AbransZ <seisuke1924@gmail.com>
2023-05-20 12:47:35 -0400

    nuevo archivo condicionales anidadas

---
+++ cond_anidadas.py
@@ -0,0 +1,16 @@
+print("Bienvenido")
+
+num = int(input("ingrese un numero entre 1 y 4 para convertirlo a palabra \n"))
+
+
+    if num == 1:
+        print("El numero", str(num), " es UNO")
+    elif num == 2:
+        print("El numero", str(num), " es DOS")
+    elif num == 3:
+        print("El numero", str(num), " es TRES")
+    elif num == 2:
+        print("El numero", str(num), "es CUATRO")
+    else:
+        print("el numero", str(num), " no es valido")
+
