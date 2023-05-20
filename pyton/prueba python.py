0db623744f8e3e9e103fe2cd4bb928b3e06588c7
AbransZ <seisuke1924@gmail.com>
2023-05-20 00:51:03 -0400

    Ejercicio en condicionales (if-else)

--- pyton/prueba python.py
+++ pyton/prueba python.py
@@ -1,31 +1,13 @@
-print("suma")
 
-
-numero_1 = 5
-numero_2 = 2
-
-suma = numero_1+numero_2
-
-
-print("el resultado de", numero_1, " mas ", numero_2, " es: ", suma)
-
-
-print("multipicacion: ")
-numero_1 = 5
-numero_2 = 2
-multi = numero_1*numero_2
-print("el resultado de", numero_1, " por ", numero_2, " es: ", multi)
-
-print("modulo: ")
-numero_1 = 5
-numero_2 = 2
-mod = numero_1 % numero_2
-print("el resultado de", numero_1, " modulo de ", numero_2, " es: ", mod)
-
-
-valor1 = int(input("Ingrese primer valor"))
-valor2 = int(input("Ingrese segundo valor"))
+valor1 = int(input("Ingrese el valor base \n"))
+valor2 = int(input("Ingrese la potencia \n"))
 
 potencia = pow(valor1, valor2)
 
-print("el resultado de la potencia es: ", str(potencia))
+if potencia >=20:
+    
+        print("el resultado de la potencia es: ", str(potencia))
+        
+else :        
+        
+        print("El resultado es Menor del valor 20 su valor es: ", str(potencia))
