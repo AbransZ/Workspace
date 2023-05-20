cb08eec8e43588a73bea3a1cb9b71777bc30213e
AbransZ <seisuke1924@gmail.com>
2023-05-20 01:24:03 -0400

    if else ejercisio sencillo

---
+++ pyton/Else-if.py
@@ -0,0 +1,13 @@
+
+valor1 = int(input("Ingrese primer valor \n"))
+valor2 = int(input("Ingrese segundo valor \n"))
+
+potencia = pow(valor1, valor2)
+
+if potencia >=20:
+    
+        print("el resultado de la potencia es: ", str(potencia))
+        
+else:        
+        
+        print("El resultado es Menor del valor 20 su valor es: ", str(potencia))
